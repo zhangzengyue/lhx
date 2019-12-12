4cf3c917ee24703bede2514142f745353c3aecec
Bigotry <3162875@qq.com>
2019-12-12 16:55:07 +0800

    队列的使用

--- app/config.php
+++ app/config.php
@@ -262,4 +262,31 @@ return [
         // 验证成功后是否重置
         'reset'    => true
     ],
+    
+    // +----------------------------------------------------------------------
+    // | 队列配置
+    // +----------------------------------------------------------------------
+    'queue'  => [
+        
+        // sync驱动表示取消消息队列还原为同步执行
+        // 'connector' => 'Sync',
+
+        // Redis驱动
+        // 'connector' => 'redis',
+        // "expire"=>60,//任务过期时间默认为秒，禁用为null
+        // "default"=>"default",//默认队列名称
+        // "host"=>Env::get("redis.host", "127.0.0.1"),//Redis主机IP地址
+        // "port"=>Env::get("redis.port", 6379),//Redis端口
+        // "password"=>Env::get("redis.password", "123456"),//Redis密码
+        // "select"=>5,//Redis数据库索引
+        // "timeout"=>0,//Redis连接超时时间
+        // "persistent"=>false,//是否长连接
+
+        // Database驱动
+        "type"=>"Database",//数据库驱动
+        "expire"=>60,//任务过期时间，单位为秒，禁用为null
+        "default"=>"default",//默认队列名称
+        "table"=>"jobs",//存储消息的表明，不带前缀
+        "dsn"=>[],
+    ]
 ];
